27d2c3cbc11fc8da61ad642a5f22bc3b8a2f9a13
Kamil <kamit2002@wp.pl>
2024-06-29 10:06:35 +0200

    init

---
+++ jaz-s26966/src/main/java/pjatk/jaz_s26966/JazS26966Application.java
@@ -0,0 +1,13 @@
+package pjatk.jaz_s26966;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS26966Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS26966Application.class, args);
+	}
+
+}
---
+++ jaz-s26966/src/test/java/pjatk/jaz_s26966/JazS26966ApplicationTests.java
@@ -0,0 +1,13 @@
+package pjatk.jaz_s26966;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS26966ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
